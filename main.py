a5d245405960612375adbaca882a5e2e6fd5052e
wilyCesde <wruizza@cesde.net>
2023-05-13 13:21:55 -0500

    cambios consultas

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ tabla1=pd.read_csv("./data/empleados.csv")
 
 from data.comidas import comidas
 
-tabla2=pd.DataFrame(comidas)
+tabla2=pd.DataFrame(comidas,columns=['nombrePlato','precioPlato'])
 
 from data.medicos import crearMedicos
 
@@ -18,7 +18,13 @@ tabla3=pd.DataFrame(medicos)
 
 # tabla1Modificada=tabla1.tail(50)
 
-tabla1Modificada=tabla1['salario']
+# tabla1Modificada=tabla1['salario',]
+
+# tabla1Modificada=tabla1[['nombres','salario']].head(10)
+
+tabla1Modificada=tabla3['nombre'].head(10)
+
+
 
 print(tabla1Modificada)
 
